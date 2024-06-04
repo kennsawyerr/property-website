432c23e9ade2fb90d8c733725b38600dba5996b8
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-06-04 20:36:29 +0100

    new file:   public/property-grid/dubai-aquarium.jpg 	new file:   public/property-grid/dubai-fountain.jpg 	new file:   public/property-grid/dubai-stadium.jpg 	modified:   src/App.css 	modified:   src/components/IdealPropertyGrid.jsx 	modified:   src/components/property-grid-files.js

--- src/components/IdealPropertyGrid.jsx
+++ src/components/IdealPropertyGrid.jsx
@@ -1,21 +1,24 @@
 
+import { propertyVideos } from "./property-grid-files"
 
 const IdealPropertyGrid = () => {
   return (
-    
     <>
-    <section className="container"
-    >
-        <h1>Discover your Ideal Property Type</h1>
+      <section className="container">
+        <h1 className="align-tx-c">Discover your Ideal Property Type</h1>
 
-        <div className="property-grid -videos">
-
-<div></div>
-
+        <div className="property-grid-videos">
+          <div className="grid-2">
+            {propertyVideos.map((property) => (
+              <div className="grid-item-2" key={property.id}>
+                <img src={property.src} alt="" />
+              </div>
+            ))}
+          </div>
         </div>
-    </section>
+      </section>
     </>
-  )
+  );
 }
 
 export default IdealPropertyGrid
--- src/components/property-grid-files.js
+++ src/components/property-grid-files.js
@@ -1,37 +1,6 @@
-export const services = [
-  {
-    id: "1",
-    link: "/H",
-    h1: "Maritime Electronics and Navigation Systems",
-    paragraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
-    src: "/marine-navigation.jpeg",
-  },
-  {
-    id: "2",
-    link: "",
-    h1: "Maritime Communication System",
-    paragraph:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt similique corrupti assumenda veritatis",
-    src: "/marine-communication.jpeg",
-  },
 
-  {
-    id: "3",
-    link: "",
-    h1: "Maritime Security System",
-    paragraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Drat?",
-    src: "/marine-security.webp",
-  },
-  {
-    id: "4",
-    link: "",
-    h1: "Information Technology and Software",
-    paragraph: "Lorem ipsum dolor t?  adipisicing elit. Drat",
-    src: "/computers.webp",
-  },
-];
 
-export const values = [
+export const property = [
   {
     p: "1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,ea libero. Lorem ipsum dolor sit amet consectetur, adipisicing elit.Corrupti ad magni facilis quidem ducimus libero tenetur, offi ciaomnis sapiente? Voluptatibus.",
     id: 1,
@@ -49,5 +18,49 @@ export const values = [
     p: "4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,ea libero. Lorem ipsum dolor sit amet consectetur, adipisicing elit.Corrupti ad magni facilis quidem ducimus libero tenetur, offi ciaomnis sapiente? Voluptatibus.",
 
     id: 4,
+  },
+];
+
+
+export const propertyVideos = [
+  {
+    id: 1,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 2,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 1,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 2,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 1,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 2,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 1,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 2,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 1,
+    src: "/property-grid/dubai-aquarium.jpg",
+  },
+  {
+    id: 2,
+    src: "/property-grid/dubai-aquarium.jpg",
   },
 ];
