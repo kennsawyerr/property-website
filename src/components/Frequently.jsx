2b6be08ecf4dc41475ba872f9920c30536fffa2b
Kehinde Sawyerr <kensawyerr@gmail.com>
2024-06-06 05:50:44 +0100

    modified:   src/App.css 	modified:   src/components/Frequently.jsx

--- src/components/Frequently.jsx
+++ src/components/Frequently.jsx
@@ -1,17 +1,29 @@
+import { IoIosArrowDown } from "react-icons/io";
 const Frequently = () => {
   return (
     <>
-      <section>
-
+      <section className="container faqs">
         <div className="clients-grid"></div>
         <div className="flex">
-            <div className="text-box">
-                <h1>Frequently Asked Questions</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consequuntur doloremque.</p>
-            </div>
-            <div className="faqs-box">
-                
-            </div>
+          <div className="text-box">
+            <h1>Frequently Asked Questions</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia,
+              consequuntur doloremque.
+            </p>
+          </div>
+          <ul className="faqs-box">
+            <li>
+              <div className="flex space-even">
+              <h3>Lorem ipsum dolor sit amet consectetur?</h3>
+              <IoIosArrowDown />
+              </div>
+              <p className="hide">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Tempore, eaque. Inventore!
+              </p>
+            </li>
+          </ul>
         </div>
       </section>
     </>
